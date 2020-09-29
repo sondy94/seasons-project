9dea435b8a5d036d837eabb70a75bfbaa44525c8
Odeson Honorat <44267745+sondy94@users.noreply.github.com>
2020-09-28 22:30:09 -0400

    create new season app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from "react";
-import "./styles.css";
+import React, { Component } from "react";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+// importing components
+import SeasonDisplay from "./components/SeasonDisplay";
+
+class App extends Component {
+  state = { lat: null };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (error) => console.log(error)
+    );
+  }
+  render() {
+    return (
+      <div>
+        <SeasonDisplay lat={this.state.lat} />
+      </div>
+    );
+  }
 }
+
+export default App;
